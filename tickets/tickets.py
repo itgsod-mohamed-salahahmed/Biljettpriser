273812bd07585eb33f6c0123bf36ab4d13ebaccf
mohamed.salahahmed <mohammed.salahahmed@elev.it-gymnasiet.se>
2015-09-15 09:43:22 +0200

    Mohamed och Mikael

--- tickets/tickets.py
+++ tickets/tickets.py
@@ -14,7 +14,22 @@ def example(argument,anotherparmeter):
     :return:
     """
 
+def alder(ar):
+    """
 
+    :param ar:
+    """
+    if ar >=0 and ar <= 17:
+        return 10
+      #print("10SEK")
+    elif ar >= 18 and ar <= 64:
+        return 20
+      #print("20SEK")
+    elif ar >= 65:
+        return 15
+      #print("15SEK")
+    else:
+      print("aldern kan inte vara negative")
 
 
 
